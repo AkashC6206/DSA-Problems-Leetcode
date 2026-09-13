59a63c8ff91f751469c442902d82e707be7378ae
Akash C <akashc6206@gmail.com>
2026-09-13 18:57:50 +0530

    Time: 27 ms (87.76%) | Memory: 108.2 MB (63.21%) - LeetSync

---
+++ Sliding-Window/219-contains-duplicate-ii/contains-duplicate-ii.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        int n = nums.length;
+        Set<Integer> s = new HashSet<>();
+        for(int i=0;i<n;i++){
+            if(!s.add(nums[i])){
+                return true;
+            }
+            if(s.size()>k){
+                s.remove(nums[i-k]);
+            }
+        }
+        return false;
+
+    }
+}
