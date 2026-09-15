8debb88bd8f64b56ee2550ff8c186b66fc8466ca
Akash C <akashc6206@gmail.com>
2026-09-15 21:26:23 +0530

    Time: 6 ms (99.19%) | Memory: 73.3 MB (5.66%) - LeetSync

--- Sliding-Window/940-fruit-into-baskets/fruit-into-baskets.java
+++ Sliding-Window/940-fruit-into-baskets/fruit-into-baskets.java
@@ -1,17 +1,23 @@
 class Solution {
     public int totalFruit(int[] fruits) {
         int n=fruits.length;
-        Map<Integer,Integer> s = new HashMap<>();
-        int maxWin = 0,i=0;
-        for(int j=0;j<n;j++){
-            s.put(fruits[j],s.getOrDefault(fruits[j],0)+1);
-            while(s.size()>2){
-                s.put(fruits[i],s.getOrDefault(fruits[i],0)-1);
-                if(s.get(fruits[i])==0) s.remove(fruits[i]);
-                i++;
+        int fruit1=-1,fruit2=-1;
+        int fruit2cnt=0,cMax=0,maxCnt=0;
+        for(int f:fruits){
+            if(f==fruit1 || f==fruit2){
+                cMax++;
+            }else{
+                cMax = fruit2cnt+1;
             }
-            maxWin = Math.max(maxWin,j-i+1);
+            if(f==fruit2){
+                fruit2cnt++;
+            }else{
+                fruit2cnt=1;
+                fruit1 = fruit2;
+                fruit2 = f;
+            }
+            maxCnt = Math.max(maxCnt,cMax);
         }
-        return maxWin;
+        return maxCnt;
     }
 }
